c0b3bfdfb1b333246e84a9aad945e505428bfdac
AkifNaveed12 <akif27031@gmail.com>
2026-07-21 14:38:09 +0500

    lesson 2 react: project running, basic files

---
+++ 01basic-react/01react-basics/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1> hello chai aur react | Muhammad Akif Naveed</h1>, <br />
+
+
+    <p>i learnt the basic file structures, some file usecases, how to set up
+      the project, run it, understanding
+      package.json and its various properties, react fragments, creating projects via vite and via react too
+    </p>
+    </>
+  
+  )
+}
+
+export default App
