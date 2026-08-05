d8ca963cacb69acfb869a4dcd3035715089a6720
AkifNaveed12 <akif27031@gmail.com>
2026-08-05 18:53:08 +0500

    tailwind css learning using react vite project

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-400 text black p-4 m-20'> Hello World</h1>
+    </>
+  )
+}
+
+export default App
