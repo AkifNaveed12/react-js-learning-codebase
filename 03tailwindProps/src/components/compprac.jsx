daab6e1f983a94c25d475c2d8e05ab6d0dc1310c
AkifNaveed12 <akif27031@gmail.com>
2026-08-05 21:28:31 +0500

    react props understanding practice 'one prop'

--- 03tailwindProps/src/components/compprac.jsx
+++ 03tailwindProps/src/components/compprac.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Compprac = (props) => {
+const Compprac = (props) => {  // componenet never changes, just the data changes through teh props
+    //we are re-using the cards
     return (
     <div>
         <h1>Hello {props.name}</h1>
